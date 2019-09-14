efc3cb03293adf3be5c2a7df61b7aab9d5f3158d
manjitha-teshara <teranteshara01@gmail.com>
2019-09-14 17:50:55 +0530

    Traveler class

---
+++ Travel_log/src/main/java/com/example/demo/Traveler.java
@@ -0,0 +1,62 @@
+package com.example.demo;
+
+import java.util.Date;
+
+public class Traveler {
+	private int travelId;
+	private String fname;
+	private String lname;
+	private String gender;
+	private String email;
+	private String mobile;
+	private Date dob;
+	private String password;
+	public int getTravelId() {
+		return travelId;
+	}
+	public void setTravelId(int travelId) {
+		this.travelId = travelId;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
