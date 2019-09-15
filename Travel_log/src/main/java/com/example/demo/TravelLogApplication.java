16bbd44c60d2643af984aa5328e93160b5f9cf62
manjitha-teshara <teranteshara01@gmail.com>
2019-09-15 07:27:12 +0530

    init

--- Travel_log/src/main/java/com/example/demo/TravelLogApplication.java
+++ Travel_log/src/main/java/com/example/demo/TravelLogApplication.java
@@ -3,12 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class TravelLogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TravelLogApplication.class, args);
 		System.out.println("Travel Log Start ...");
+		
+		
 	}
 
 }
