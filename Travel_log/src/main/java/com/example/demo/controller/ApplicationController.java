c30d376bbba124c3fa3322cdc2544e700380e5ec
manjitha-teshara <teranteshara01@gmail.com>
2019-09-15 17:05:52 +0530

    html page view init

--- Travel_log/src/main/java/com/example/demo/controller/ApplicationController.java
+++ Travel_log/src/main/java/com/example/demo/controller/ApplicationController.java
@@ -1,16 +1,23 @@
 package com.example.demo.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.ModelMap;
 
-@RestController
+//@RestController
 @Controller
 public class ApplicationController {
 	@ResponseBody
 	@RequestMapping("/home")
 	public String Hello() {
-		return "Hello Travel Log start";
+		return "Hello Travel Log start 1";
+	}
+	
+	@RequestMapping("/welcome")
+	public String Welcome() {
+		return "welcomepage";
 	}
 }
